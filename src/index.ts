5f1a259c1e0aacbb65e4300ef5d2e2bb72cf89d4
xiaomingTang <1038761793@qq.com>
2019-11-30 02:20:58 +0800

    add the welcome page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const add = (a: number, b: number) => a + b
+import "./index.scss"
 
-console.log(add(5, 3))
+const app = document.querySelector("#__tampermonkey__app")
+
+const innerHTML = `
+<div class="f13">
+  某度中的<a href="javascript:;" class="__old-style">广告</a>字样经过处理后就是这样了:
+  <a href="javascript:;" class="m">广告</a>
+</div>
+`
+
+if (app) {
+  app.innerHTML = innerHTML
+}
